7c5382663b8fefb5dd1018b349b7dcf04d828192
Keita Kasai <k013c1345@it-neec.jp>
2017-09-09 22:55:08 +0900

    debug

--- src/test/java/TestDescription.java
+++ src/test/java/TestDescription.java
@@ -29,8 +29,8 @@ public class TestDescription extends TestWatcher {
 //        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 //        driver = new FirefoxDriver();
 //        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        System.out.println(Paths.get("./").toAbsolutePath());
-        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        System.out.println(Paths.get("chromedriver").toAbsolutePath());
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
         driver = new ChromeDriver();
         System.out.println("TestDescription starting");
     }

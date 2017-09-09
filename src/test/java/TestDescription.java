34921880b02de40f32a2d9a22570815ea76b035d
Keita Kasai <k013c1345@it-neec.jp>
2017-09-09 22:37:48 +0900

    add driver

--- src/test/java/TestDescription.java
+++ src/test/java/TestDescription.java
@@ -29,7 +29,7 @@ public class TestDescription extends TestWatcher {
 //        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 //        driver = new FirefoxDriver();
 //        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
         driver = new ChromeDriver();
         System.out.println("TestDescription starting");
     }
